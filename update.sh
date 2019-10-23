d26ea33fe4868b22b03cdc0a34ae2524383929da
Matthew McEachen <matthew@photostructure.com>
2019-10-23 11:28:12 -0700

    Replace updater with a shell script. Update to 11.73.

---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -ex
+
+# Updates to the latest version of exiftool. Assumes we can git clone exiftool
+# into the parent directory.
+
+if [ ! -d ../exiftool ] ; then
+  (cd .. ; git clone git@github.com:exiftool/exiftool.git)
+fi
+
+(cd ../exiftool ; git add . ; git stash ; git checkout master ; git pull)
+
+rm -rf bin
+mkdir -p bin
+cp -rp ../exiftool/* bin
+rm -rf bin/t bin/html bin/exiftool-master
+
+# exiftool never has a patch version:
+NEWVER=$(bin/exiftool -ver).0
+
+yarn version --new-version $NEWVER
