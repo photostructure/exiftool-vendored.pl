e23c69c377707eb1b0711bf24066f5795c358709
Matthew McEachen <matthew@photostructure.com>
2022-06-25 13:49:27 -0700

    shfmt and delint update.sh. no-op if no pending changes.

--- update.sh
+++ update.sh
@@ -3,18 +3,31 @@
 # Updates to the latest version of exiftool. Assumes we can git clone exiftool
 # into the parent directory.
 
-if [ ! -d ../exiftool ] ; then
-  (cd .. ; git clone git@github.com:exiftool/exiftool.git)
+if [ ! -d ../exiftool ]; then
+  (
+    cd ..
+    git clone git@github.com:exiftool/exiftool.git
+  )
 fi
 
-(cd ../exiftool ; git add . ; git stash ; git checkout master ; git pull)
+(
+  cd ../exiftool
+  git add .
+  git stash -u
+  git checkout master
+  git pull
+)
 
 rm -rf bin
 mkdir -p bin
 cp -rp ../exiftool/* bin
 rm -rf bin/t bin/html bin/windows_exiftool
 
-# exiftool never has a patch version:
-NEWVER=$(bin/exiftool -ver).0-pre
-
-yarn version --new-version $NEWVER
+# Are there pending updates?
+if [ "$(git status --porcelain=v1 2>/dev/null | wc -l)" -eq 0 ]; then
+  echo "No-op: already up to date"
+else
+  # exiftool never has a patch version:
+  NEWVER=$(bin/exiftool -ver).0-pre
+  yarn version --new-version "$NEWVER"
+fi
